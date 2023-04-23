98befc27a24035c5beeb56ebb8477c5c19020f5d
Gleidson De Sousa <desousag2000@gmail.com>
2023-04-22 20:52:57 -0400

    Initial Commit

---
+++ Verify/VerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerifyApp.swift
+//  Verify
+//
+//  Created by Gleidson De Sousa on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct VerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
